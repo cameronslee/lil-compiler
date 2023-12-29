07927ab554330b8bf5b15251b000b6f4ee42559c
Cameron Lee <cameronslee@hotmail.com>
2023-12-29 15:19:05 -0800

    init

---
+++ source.c
@@ -0,0 +1,39 @@
+// C Compiler
+
+size_t get_file_size(FILE *f) {
+  size_t file_size;
+  if (fseek(f, 0, SEEK_END) != 0 ) exit(EXIT_FAILURE); 
+
+  file_size = ftell(f);
+  rewind(f);
+  return file_size;
+}
+
+void read_file(FILE * f, char *buffer, size_t file_size) {
+  if (file_size == 1) exit(EXIT_FAILURE);
+
+  fread(buffer, 1, file_size, f);
+  if (buffer == NULL) exit(EXIT_FAILURE);
+
+  fclose(f); 
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("%s\n", "usage: foo <filename>");
+    exit(1);
+  }
+
+  FILE *f;
+  if(!(f = fopen(argv[1], "r"))) {
+    printf("%s\n", "error: file not found");
+    exit(1);
+  }
+
+  size_t file_size = get_file_size(f);
+  char *buffer = malloc(file_size * sizeof(char));
+
+  read_file(f, buffer, file_size);
+
+  printf("%s\n", buffer); 
+}
